88df1d00c033830339c9731592043de1d592b008
Luuk Vink <112398283+Luukvink28@users.noreply.github.com>
2023-01-25 14:36:09 +0100

    changes
    hoi

---
+++ Quiz-80s-rap/script.js
@@ -0,0 +1,43 @@
+function inleveren() {
+    var goed = 0;
+    var fout = 0;
+    var q1c = document.getElementById("q1c");
+    var q2b = document.getElementById("q2b");
+    var q3c = document.getElementById("q3c");
+    var q4a = document.getElementById("q4a");
+    if (q1c.checked) {
+        goed++;
+        document.getElementById("a1G").innerHTML = "Goed!";
+    } else {
+        fout++;
+        document.getElementById("a1F").innerHTML = "Fout! Het juiste antwoord was C";
+    }
+    if (q2b.checked) {
+        goed++;
+        document.getElementById("a2G").innerHTML = "Goed!";
+    } else {
+        fout++;
+        document.getElementById("a2F").innerHTML = "Fout! Het juiste antwoord was B";
+    }
+    if (q3c.checked) {
+        goed++;
+        document.getElementById("a3G").innerHTML = "Goed!";
+    } else {
+        fout++;
+        document.getElementById("a3F").innerHTML = "Fout! Het juiste antwoord was C";
+    }
+    if (q4a.checked) {
+        goed++;
+        document.getElementById("a4G").innerHTML = "Goed!";
+    } else {
+        fout++;
+        document.getElementById("a4F").innerHTML = "Fout! Het juiste antwoord was A";
+    }
+    document.getElementById("resultaten").innerHTML = "Goed: " + goed + "<br>Fout: " + fout;
+    document.getElementById("inleverButton").style.display = "none";
+    document.getElementById("resetButton").style.display = "block";
+}
+
+function reset() {
+    window.location.reload();
+}
