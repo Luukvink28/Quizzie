1217a4d0bcf08b7251d7ccb3c1bc16508b1891bf
Loenka123 <122457367+Loenka123@users.noreply.github.com>
2023-02-15 11:46:58 +0100

    Contact
    Praat met emmet

---
+++ Contact/Email.php
@@ -0,0 +1,22 @@
+<?php
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $to = "jljnootenboom@gmail.com";
+    $subject = "Quizzie - Nieuw bericht";
+    $message = "Er is een nieuw bericht: " . $_POST['message'] . " Het email adres is: " . $_POST['email'] . " De naam is: " . $_POST['name'];
+    $headers = "Van: " . $_POST['email'];
+    mail($to, $subject, $message, $headers);
+}
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Bedankt!</title>
+    <link rel="stylesheet" href="https://unpkg.com/@picocss/pico@1.*/css/pico.min.css">
+</head>
+<body>
+    Bedankt voor het versturen van uw bericht.
+</body>
+</html>
